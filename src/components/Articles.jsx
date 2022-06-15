310f2baaa89e5ab36f528f471a6763191ab20043
DanielEgidio <danielegidio@si.fiponline.edu.br>
2022-06-15 16:17:41 -0300

    Criação e setup da aplicação

---
+++ src/components/Articles.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Articles = () => {
+  return (
+    <div className="articles">
+      <h2>Artigos</h2>
+    </div>
+  );
+};
+
+export default Articles;
---
+++ src/components/User.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Usuários = () => {
+  return (
+    <div className="user">
+      <h2>Usuários</h2>
+    </div>
+  );
+};
+
+export default Usuários;
